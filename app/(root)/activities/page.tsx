9b8586d73c50de4517561eed0916306b52553675
Carlos Vargas Arce <woxry@Carloss-MacBook-Pro.local>
2023-12-04 13:51:41 -0600

    Sixth delivery

---
+++ app/(root)/activities/page.tsx
@@ -0,0 +1,20 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    return (
+        <section>
+           <h1 className="pr_head-text-dark text-left">Activities</h1>
+           <p>Coming Soon</p>
+        </section>
+    )
+}
+
+export default Page
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import PersonalRecords from "../models/personalRecords.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
     userId: string;
@@ -78,5 +79,55 @@ export async function fetchUserPersonalRecords(userId: string) {
         return personalRecords;
     } catch (error: any) {
         throw new Error(`Failed to fetch user personal records: ${error.message}`)
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20, 
+    sortBy = "desc"
+}: {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder 
+}) {
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber -1) * pageSize;
+
+        const regex = new RegExp(searchString, 'i');
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId}
+        }
+
+        if(searchString.trim() !== '') {
+            query.$or = [
+                { username: {$regex: regex }},
+                { name: {$regex: regex }}
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy };
+
+        const usersquery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize)
+        
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersquery.exec();
+
+        const isNext = totalUsersCount > skipAmount + users.length;
+
+        return { users, isNext}
+    } catch (error : any) {
+        throw new Error(`Failed to fetch users: ${error.message}`)
     }
 }
